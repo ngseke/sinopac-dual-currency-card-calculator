250f712834e421399ac879a5d893898d9fb97947
Huang Xingqiao <a92304a92304@gmail.com>
2021-06-22 00:09:38 +0800

    fix avgRate display

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -44,7 +44,7 @@ form.flex.flex-wrap.-mx-4
       div
         .title 平均回饋率
         .text-3xl.font-bold.text-gray-700(class='dark:text-gray-300')
-          template(v-if='avgRate') {{ avgRate }}%
+          template(v-if='avgRate != null && !isNaN(avgRate)') {{ avgRate }}%
           template(v-else) -
 
       div
