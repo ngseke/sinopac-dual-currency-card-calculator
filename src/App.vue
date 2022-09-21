5b84c7dbd541e0843171f1a0b0e622168a40a7c8
ngseke <ngseke@gmail.com>
2022-09-21 23:58:43 +0800

    feat: add disclaimer

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,21 @@
     TheHeader
     Calculator
     TheFooter
+
+Teleport(to='body')
+  transition(leave-to-class='translate-y-full opacity-0')
+    button.fixed.bottom-5.left-4.border-4.border-white.rounded-lg.p-1.transform.-rotate-12.transition-all(
+      v-if='isDisclaimerVisible'
+      @click='isDisclaimerVisible = false'
+    )
+      div.text-white.font-black.text-center.text-lg
+        | 一代神卡
+        br
+        span.text-2xl 已殞落
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 
 import Calculator from './components/Calculator.vue'
 import TheHeader from './components/TheHeader.vue'
@@ -18,6 +29,8 @@ onMounted(() => {
     document.body.className += 'transition-colors duration-300'
   }, 300)
 })
+
+const isDisclaimerVisible = ref(true)
 </script>
 
 <style lang="sass">
